86ca27fff8d1ad67cfe4c26822f991d3e98d6dc4
Eder Soares <edersoares@me.com>
2018-01-11 16:20:44 -0200

    Turn api_token column as nullable in migration and hidden in User model

--- database/migrations/2018_01_11_030416_add_column_api_token_in_users_table.php
+++ database/migrations/2018_01_11_030416_add_column_api_token_in_users_table.php
@@ -14,7 +14,7 @@ class AddColumnApiTokenInUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('api_token');
+            $table->string('api_token')->nullable();
         });
     }
 
