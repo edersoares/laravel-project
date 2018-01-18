6de7aa4592fae0a852688634c1e5b324e5b09cff
Eder Soares <edersoares@me.com>
2018-01-18 17:09:53 -0200

    Updated factories

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,7 +3,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Category::class, function (Faker $faker) {
+
+    $slug = str_slug($name = $faker->word);
+
     return [
-        //
+        'category_id' => null,
+        'name' => $name,
+        'slug' => $slug,
+        'boolean' => $faker->boolean,
     ];
 });
--- database/factories/UserGroupFactory.php
+++ database/factories/UserGroupFactory.php
@@ -3,7 +3,12 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\UserGroup::class, function (Faker $faker) {
+
+    $slug = str_slug($name = $faker->word);
+
     return [
-        //
+        'name' => $name,
+        'slug' => $slug,
+        'boolean' => $faker->boolean,
     ];
 });
