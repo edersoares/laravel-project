681bfb0b4ec10ec3f2f6b60dfa728345ff7a45c2
Eder Soares <edersoares@me.com>
2018-01-22 18:44:15 -0200

    Casts in models

--- app/Tag.php
+++ app/Tag.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'category_id' => 'integer',
+        'active' => 'boolean'
+    ];
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
--- app/UserGroup.php
+++ app/UserGroup.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class UserGroup extends Model
 {
     /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'active' => 'boolean'
+    ];
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
