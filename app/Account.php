2b16537be5d4c8f82449c35173fbe3e9c6a37934
Eder Soares <edersoares@me.com>
2018-01-22 18:41:30 -0200

    Account model casts and fillable

--- app/Account.php
+++ app/Account.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    //
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'active' => 'boolean'
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug', 'active'
+    ];
 }
