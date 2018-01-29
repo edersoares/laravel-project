534bf2e1af435f451fc0855eb134f1ab07f04a94
Eder Soares <edersoares@me.com>
2018-01-29 20:26:18 -0200

    Add in isAdmin() method ind User

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token', 'api_token'
     ];
+
+    /**
+     * Return if user is admin.
+     *
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return $this->getKey() === 1;
+    }
 }
