33b53d7fe910a1dbeba7cc44081b503cc96d61b1
Eder Soares <edersoares@me.com>
2018-01-30 15:18:03 -0200

    Creates a macro to Route facade called "api"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Route::macro('api', function ($endpoint, $controller) {
+            Route::get($endpoint, "{$controller}@index")->name("{$endpoint}.index");
+            Route::post($endpoint, "{$controller}@create")->name("{$endpoint}.create");
+            Route::get("{$endpoint}/{id}", "{$controller}@browse")->name("{$endpoint}.browse");
+            Route::put("{$endpoint}/{id}", "{$controller}@update")->name("{$endpoint}.update");
+            Route::delete("{$endpoint}/{id}", "{$controller}@delete")->name("{$endpoint}.delete");
+        });
     }
 
     /**
