b4e4ffe6388b408ccb86a4e26d20572f51866fbc
Eder Soares <edersoares@me.com>
2018-02-01 17:51:27 -0200

    Elasticsearch

--- app/Account.php
+++ app/Account.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Account extends Model
 {
+    use Searchable;
+
     /**
      * The attributes that should be cast to native types.
      *
