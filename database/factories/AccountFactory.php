f2a15ffb2016d8e3c44accd6d153676b4ba89572
Eder Soares <edersoares@me.com>
2018-02-14 20:25:15 -0200

    Fix truncate name in Account factory

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Account::class, function (Faker $faker) {
 
-    $slug = str_slug($name = $faker->company);
+    $slug = str_slug($name = substr($faker->company, 0, 32));
 
     return [
         'name' => $name,
