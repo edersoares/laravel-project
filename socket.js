85dcf1a13e0d17f83e799f37808515691e3c0006
Eder Soares <edersoares@me.com>
2018-02-18 14:48:37 -0300

    Creates socket service

---
+++ socket.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Redis = require('ioredis');
+var redis = new Redis(6379, 'redis');
+
+redis.subscribe('test-channel', function(err, count) {
+    console.log('Connected.');
+});
+
+redis.on('message', function(channel, message) {
+    console.log('Message Recieved: ' + message);
+    message = JSON.parse(message);
+    io.emit(channel + ':' + message.event, message.data);
+});
+
+http.listen(3000, function(){
+    console.log('Listening on Port 3000');
+});
